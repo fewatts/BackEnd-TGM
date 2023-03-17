8215c704fddabcc3046d640b9037b6614a75c0b8
Fewatts <feralveswatts@gmail.com>
2023-03-17 14:31:25 -0300

    Base do projeto (Falta criar controller, model e repository)

---
+++ src/main/java/com/generation/thegreenmarket/ThegreenmarketApplication.java
@@ -0,0 +1,13 @@
+package com.generation.thegreenmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThegreenmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThegreenmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/thegreenmarket/ThegreenmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.thegreenmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThegreenmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
