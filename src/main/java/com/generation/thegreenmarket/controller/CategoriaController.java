53b7730e4923ca0f454ce73bcfba38ada9754251
unknown <erika.f55@hotmail.com>
2023-03-22 14:41:45 -0300

    Post CategoriaController

--- src/main/java/com/generation/thegreenmarket/controller/CategoriaController.java
+++ src/main/java/com/generation/thegreenmarket/controller/CategoriaController.java
@@ -2,12 +2,16 @@ package com.generation.thegreenmarket.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,4 +42,8 @@ public class CategoriaController {
 		return ResponseEntity.ok(categoriaRepository.findAllByNomeCategoriaContainingIgnoreCase(categoria));
 	}
 
+	@PostMapping
+	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+	}
 }
