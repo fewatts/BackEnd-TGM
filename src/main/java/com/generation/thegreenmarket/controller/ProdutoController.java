4035031ec308d946f30dc9b269377042934a3ae4
unknown <erika.f55@hotmail.com>
2023-03-23 15:29:45 -0300

    Task-5 parte-2
    ProdutoController metodo Delete

--- src/main/java/com/generation/thegreenmarket/controller/ProdutoController.java
+++ src/main/java/com/generation/thegreenmarket/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package com.generation.thegreenmarket.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,13 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.thegreenmarket.model.Produto;
 import com.generation.thegreenmarket.repository.CategoriaRepository;
@@ -68,6 +72,17 @@ public class ProdutoController {
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Produto> produto = produtoRepository.findById(id);
+		
+		if(produto.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		produtoRepository.deleteById(id);
 	}
 
 }
